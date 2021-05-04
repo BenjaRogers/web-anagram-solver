191f0aafd507cfee6b40a27c8e0bddfd6797374e
BenjaRogers <64092117+BenjaRogers@users.noreply.github.com>
2021-05-04 18:41:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def get_post():
         anagram_string += word + " "
     return anagram_string
 if __name__ =='__main__':
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
